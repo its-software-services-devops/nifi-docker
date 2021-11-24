48372d77b8b2f6623dd1834568082c79345cd6d4
seubpong.mon <pjame.fb@gmail.com>
2021-11-24 12:27:56 +0700

    Test add custom script

--- custom.bash
+++ custom.bash
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-echo "SEUB is great!!!"
-# Do something here!!!
+prop_replace () {
+  target_file=${3:-${nifi_props_file}}
+  echo "Custom replacing [$1] to [$2] target file ${target_file}"
+  sed -i -e "s|^$1=.*$|$1=$2|"  ${target_file}
+}
+
+prop_replace 'nifi.web.proxy.host' "${NIFI_WEB_PROXY_HOST}"
 
 ../scripts/start.sh
